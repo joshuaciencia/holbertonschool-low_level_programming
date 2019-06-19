48b25778c835170f8497d14f140d220d28beb03e
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-19 19:48:37 +0000

    missing blank line after declaration

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(str));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof(str));
 	return (1);
 }
