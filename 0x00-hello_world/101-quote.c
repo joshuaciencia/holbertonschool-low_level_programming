83c379576e932d0266f196dbcca3d1c5df0db284
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-19 19:51:14 +0000

    added blank line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,9 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[];
+
+	str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 sizeof(str));
 	return (1);
