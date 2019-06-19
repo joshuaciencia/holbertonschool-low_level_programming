02e4e80ebc99e50c106afa7e97d50da1434e2787
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-19 16:56:49 +0000

    advanced 9 write to buffer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,9 @@
-#include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int i;
-	int size = sizeof(str) / sizeof(str[0]);
-	for(i = 0; i < size; i++){
-		/*putchar((char)str[i]);*/
-	}
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	/*putchar('\n');*/
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(str));
 	return 1;
 
 }
