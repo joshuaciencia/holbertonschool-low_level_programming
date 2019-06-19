70d71893f5167c9de87b8ca891796e4752067612
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-19 18:40:14 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - print some text
+ * Return: 0
+ */
 int main(void)
 {
- printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
-
- return (0);
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
 
