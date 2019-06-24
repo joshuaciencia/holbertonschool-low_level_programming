8091f08cf228551439b38f1b360c4d13a205d61b
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-24 20:31:56 +0000

    is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,25 @@
-k
+#include "holberton.h"
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int r = _islower('y');
+	_putchar('\n');
+	return (0);
+}
+/**
+ * _islower - checks for lowercase letters
+ * Return: void
+ * @c: ascii code to be checked
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+
+	return (0);
+}
