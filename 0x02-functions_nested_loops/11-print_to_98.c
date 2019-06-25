e5e8f7ad447862296353652e5ed6115a9713eb27
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-25 15:20:39 +0000

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,5 @@
 #include "holberton.h"
-#include <stdbool.h>
-/**
- * main - test
- * Return: 0
- */
-int main(void)
-{
-	print_to_98(5);
-	_putchar('\n');
-	return (0);
-}
+#include <stdio.h>
 /**
  * print_to_98 - prints natural numbers to n
  * Return: the absolute value of an integer
@@ -21,45 +11,14 @@ void print_to_98(int n)
 
 	while (i != 98)
 	{
-
-		int whole = i;
-
-		int digits = 0;
-
-		int j;
-
 		if (i > 98)
 			i--;
 		else
 			i++;
-
-		while (true)
-		{
-			if (whole < 1)
-				break;
-			whole = whole / 10;
-			digits++;
-		}
-
-		for (j = 0; j < digits; j++)
-		{
-
-			int pow = 1;
-
-			int c;
-
-			for (c = 0; c < digits - 1; c++)
-			{
-				pow = pow * 10;
-			}
-
-			_putchar(48 + (i / pow) % 10);
-		}		
-		
 		if (i != 98)
-		{
-			_putchar(44);
-			_putchar(' ');
-		}
+			printf("%d, ", i);
+		else
+			printf("%d", i);
 	}
+	printf("\n");
 }
