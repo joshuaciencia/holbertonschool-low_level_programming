65e245d513ee9f3090626c4ecfa25d6fad4dcf8c
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-25 02:18:21 +0000

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * main - test
+ * Return: 0
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
+/**
+ * jack_bauer - prints all minutes
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			int h1 = i / 10;
+
+			int h2 = i % 10;
+
+			int m1 = j / 10;
+
+			int m2 = j % 10;
+
+			_putchar(48 + h1);
+			_putchar(48 + h2);
+			_putchar(58);
+			_putchar(48 + m1);
+			_putchar(48 + m2);
+			_putchar('\n');
+		}
+	}
+}
