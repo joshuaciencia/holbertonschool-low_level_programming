29c4dd3b19e54b33ab0bb0a4be590ebda4a7a336
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-25 02:47:50 +0000

    learn your times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+/**
+ * main - test
+ * Return: 0
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
+/**
+ * times_table - prints all minutes
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int n = j * i;
+
+			int n1 = n / 10;
+
+			int n2 = n % 10;
+
+			if (n < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(48 + n);
+			} else
+			{
+				_putchar(' ');
+				_putchar(48 + n1);
+				_putchar(48 + n2);
+			}
+
+			if (j != 9)
+				_putchar(44);
+
+		}
+		_putchar('\n');
+	}
+}
