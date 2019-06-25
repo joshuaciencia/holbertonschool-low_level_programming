afd50dcb854697827dd74514105c2969675bb686
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-25 05:37:57 +0000

    body in header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,7 +2,20 @@
 #define HOLBERTON
 
 int _putchar(char c);
-void print_alphabet(void);
+/**
+ * print_alphabet - prints alphabet
+ * Return: the result of the adition
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
