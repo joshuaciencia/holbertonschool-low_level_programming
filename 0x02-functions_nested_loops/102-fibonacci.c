e545afa8bf983c2414589631f16b54c782e8d86e
joshuaciencia <joshuaciencia2016@gmail.com>
2019-06-26 04:16:36 +0000

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,25 +12,26 @@ int main(void)
 
 void f(int n)
 {
-	unsigned long long n1 = 1;
+	long long n1 = 1;
 
-	unsigned long long n2 = 2;
+	long long n2 = 2;
 
 	int i;
 
 	printf("%llu, ", n1);
+	printf("%llu\n", n2);
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n - 2; i++)
 	{
-
-		if (i == n - 2)
-			printf("%llu", n2);
-		else
-			printf("%llu, ", n2);
-		unsigned long long n3 = n1 + n2;
+		long long n3 = n1 + n2;
 
 		n1 = n2;
 		n2 = n3;
+
+		if (i == n - 3)
+			printf("%llu", n2);
+		else
+			printf("%llu, ", n2);
 	}
 	printf("\n");
 }
