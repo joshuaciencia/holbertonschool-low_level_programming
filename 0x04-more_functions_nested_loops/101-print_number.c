419dd9bd34eed4e7f12d5cf62d2e026a8cda8019
SantiagoHerreG <888@holbertonschool.com>
2019-06-27 23:07:52 +0000

    unsigned

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,14 @@
  */
 void print_number(int n)
 {
-	int size = n;
+	unsigned int size = n;
 
 	int digits = 0;
 
 	int i;
 
 	if (n < 0)
-		size = -size;
+		size = -n;
 	if (n == 0)
 		digits = 1;
 
