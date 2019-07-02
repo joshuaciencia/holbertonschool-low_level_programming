c0588c8827f6722adb21bbc5ce06107a3e850eff
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-02 14:36:20 +0000

    exercise 7

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -2,9 +2,9 @@
 #include "holberton.h"
 
 int main(void){
-        char *str;
+    char *str;
 
-    str = "0123456789";
-    puts2(str);
+    str = "123456789";
+    puts_half(str);
     return (0);
 }
