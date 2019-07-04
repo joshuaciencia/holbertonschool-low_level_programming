88e9ba275cef0816f2dc59e433c021db642b4900
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-04 23:08:28 +0000

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,15 @@ char *leet(char *s)
 {
 	int i, j;
 
-	char lower[5] = {'a', 'e', 'o', 't', 'l'};
+	char lower[] = {'a', 'e', 'o', 't', 'l'};
 
-	char upper[5] = {'A', 'E', 'O', 'T', 'L'};
+	char upper[] = {'A', 'E', 'O', 'T', 'L'};
 
-	char value[5] = {'4', '3', '0', '7', '1'};
+	char value[] = {'4', '3', '0', '7', '1'};
 
 	while (s[i] != 0)
 	{
 		char c = s[i];
-
-		/* check for lowercase */
 
 		for (j = 0; j < 5; j++)
 		{
