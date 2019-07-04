2f2dee41b4c046c6cffc23a6de0fb4419c23502b
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-04 18:42:04 +0000

    first task 0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,24 @@
 #include "holberton.h"
 /**
- * reset_to_98 - updates value
- * Return: void
- * @n: number to be proccesed
+ * _strcat - concatenates two strings
+ * @dest: dest string
+ * @src: src string
+ * Return: pointer to resulting string
  */
-
+char *_strcat(char *dest, char *src)
 {
-	*n = 98;
+	int s = 0, i = 0;
+
+	while (*(dest + s) != 0)
+	{
+		s++;
+	}
+	while (*(src + i) != 0)
+	{
+		*(dest + s + i)	= *(src + i);
+		i++;
+	}
+
+	*(src + i + s) = 0;
+	return (dest);
 }
