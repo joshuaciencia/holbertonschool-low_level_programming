0970b314623a50fa04f8cdc618f4f944a12e6108
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-04 21:56:54 +0000

    task 5

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,23 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
 
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-
 /**
  * main - check the code for Holberton School students.
  *
@@ -25,10 +8,11 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+    char s[] = "Holberton School!\n";
+    char *p;
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
+    p = string_toupper(s);
+    printf("%s", p);
+    printf("%s", s);
     return (0);
 }
