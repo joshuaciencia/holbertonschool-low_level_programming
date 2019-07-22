982b848a444d06aa1dde49276f1087d5caaf025b
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-22 19:40:53 +0000

    free both fileds as well

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,13 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * free_dog - frees a god struct type
+ * free_dog - frees a dog struct type
  * @d: type struct
  * Return: void
  */
 void free_dog(dog_t *d)
 {
+	free(d.name)
+	free(d.owner);
 	free(d);
 }
