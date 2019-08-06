95ed7994eb43b8898e65c7178affb7806d54d699
SantiagoHerreG <888@holbertonschool.com>
2019-08-06 21:35:39 +0000

    fixed for null head

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,23 +8,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int size = 0, i, iterator = 1;
+	int size = 0, i;
 	const listint_t *tmp[100];
 
-	while (head && iterator)
+	if (!head)
+		exit(98);
+
+	while (head)
 	{
-		tmp[size] = head;
-		size++;
 		printf("[%p] %d\n", (void *)head, head->n);
 		for (i = 0; i < size; i++)
 		{
-			if (tmp[i] == head->next)
+			if (tmp[i] == head)
 			{
-				iterator = 0;
-				printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-				break;
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				return (size);
 			}
 		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		tmp[i] = head;
+		size++;
 		head = head->next;
 	}
 	return (size);
