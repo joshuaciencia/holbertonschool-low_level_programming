8cacd67e8181e2b625b018b64e067183a4254212
SantiagoHerreG <888@holbertonschool.com>
2019-08-06 20:54:30 +0000

    fixed for null node

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,7 +11,7 @@ size_t print_listint_safe(const listint_t *head)
 	int size = 0, i, iterator = 1;
 	const listint_t *tmp[100];
 
-	if (!head)
+	if (!*head)
 		exit(98);
 
 	while (head && iterator)
