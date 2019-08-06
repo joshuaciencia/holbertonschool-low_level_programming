dd02b023823e47c8c433ee4471d6eb4c43b39355
SantiagoHerreG <888@holbertonschool.com>
2019-08-06 21:40:27 +0000

    fixed size index

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -25,7 +25,7 @@ size_t print_listint_safe(const listint_t *head)
 			}
 		}
 		printf("[%p] %d\n", (void *)head, head->n);
-		tmp[i] = head;
+		tmp[size] = head;
 		size++;
 		head = head->next;
 	}
