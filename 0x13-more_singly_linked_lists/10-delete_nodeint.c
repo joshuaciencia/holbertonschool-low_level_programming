c6b38437700849fc5f3ec7f44496eae24924ca0e
josh <joshuaciencia2016@gmail.com>
2019-08-06 11:23:20 +0000

    removed trailing withe space

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = node->next;
 	}
 	free(node);
-	return (1);	
+	return (1);
 }
 /**
  * get_nodeint_at_index - get node at index
