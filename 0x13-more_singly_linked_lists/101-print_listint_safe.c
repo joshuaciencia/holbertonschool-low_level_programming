e3ed6a02d2315c825c7eee276b731a89e97767b3
SantiagoHerreG <888@holbertonschool.com>
2019-08-06 21:14:08 +0000

    testing

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,11 +11,11 @@ size_t print_listint_safe(const listint_t *head)
 	int size = 0, i, iterator = 1;
 	const listint_t *tmp[100];
 
-	if (!*head)
+	if (!head)
 		exit(98);
 
 	while (head && iterator)
-	{
+	{	
 		tmp[size] = head;
 		size++;
 		printf("[%p] %d\n", (void *)head, head->n);
