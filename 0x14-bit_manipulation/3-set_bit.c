d0032ff6bb242ba23eb417b8d96387cb3eb8ffc0
joshuaciencia <joshuaciencia2016@gmail.com>
2019-08-08 16:29:43 +0000

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,9 +7,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned int copy;
 
+	copy = *n;
+	copy = get_bit(copy, index);
 
+	if (copy == 0x1)
+		return (-1);
 	*n = (*n) + power(2, index);
 	return (1);
 }
@@ -31,4 +35,19 @@ unsigned int power(unsigned int base, unsigned int exp)
 	for (i = 0; i < exp; i++)
 		raised = raised * base;
 	return (raised);
+}
+/**
+ * get_bit - get value of bit at index
+ * Return: value
+ * @n: num
+ * @index: index given
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int c;
+
+	for (c = 0; c < index; c++)
+		n >>= 0x1;
+
+	return (n & 0x1);
 }
