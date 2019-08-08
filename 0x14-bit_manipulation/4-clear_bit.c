8ba8259ef77cced41878dfbf6324f31317686b0b
joshuaciencia <joshuaciencia2016@gmail.com>
2019-08-08 17:12:57 +0000

    added readme

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
-
+/**
+ * clear_bit - set bit to 0 at index
+ * Return: 1 if worked, -1 if failed
+ * @n: pointer to number
+ * @index: given index
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	
