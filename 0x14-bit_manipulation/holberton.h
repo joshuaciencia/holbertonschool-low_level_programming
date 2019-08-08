e74f5f2e824dc1690085d38de2d5a5e3cf524387
joshuaciencia <joshuaciencia2016@gmail.com>
2019-08-08 20:00:53 +0000

    task 100 endianness

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -9,4 +9,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 unsigned int _strlen(const char *str);
 unsigned int power(unsigned int base, unsigned int exp);
+int get_endianness(void);
 #endif
