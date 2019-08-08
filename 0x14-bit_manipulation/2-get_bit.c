93398edeea50dd82beadbc3fcb1baeb5dcd082ae
joshuaciencia <joshuaciencia2016@gmail.com>
2019-08-08 16:26:38 +0000

    added 1, 2 task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,16 @@
 #include "holberton.h"
-
+/**
+ * get_bit - get value of bit at index
+ * Return: value
+ * @n: num
+ * @index: index given
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	
+	unsigned int c;
+
+	for (c = 0; c < index; c++)
+		n >>= 0x1;
+
+	return (n & 0x1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,34 @@
 #include "holberton.h"
-
+/**
+ * set_bit - set bit to 1 at index
+ * Return: 1 if worked, -1 if failed
+ * @n: pointer to number
+ * @index: given index
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	
+
+
+	*n = (*n) + power(2, index);
+	return (1);
+}
+/**
+ * power - power of base
+ * @base: base
+ * @exp: exponent
+ * Return: base raised to exp
+ */
+unsigned int power(unsigned int base, unsigned int exp)
+{
+	unsigned int i = 0;
+	unsigned int raised = 1;
+
+
+	if (exp == 0)
+		return (1);
+
+	for (i = 0; i < exp; i++)
+		raised = raised * base;
+	return (raised);
 }
