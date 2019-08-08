bee7633c928babf1852b4190c2dec80783a61f91
joshuaciencia <joshuaciencia2016@gmail.com>
2019-08-08 19:00:58 +0000

    fixed for null string

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec, size, i, exp;
 
+	if (!b)
+		return (0);
 	size = _strlen(b);
 	i = 0;
 	dec = 0;
 	exp = size - 1;
-
-	if (!b || size > 64)
-		return (0);
 
 	while (b[i])
 	{
