88a4bce0ce241ce8347ce8ea221db3f2e4bbe4f4
joshuaciencia <joshuaciencia2016@gmail.com>
2019-08-13 19:25:43 +0000

    init repo

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads and prints from a file
+ * @filename: path to file
+ * @letters: chars to read
+ * Return: chars read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	unsigned int count = 0;
+	int ch;
+
+	if (!filename)
+		return (0);
+
+	file = fopen(filename, "r");
+
+	if (!file)
+		return (0);
+
+	while ((ch = fgetc(file)) != EOF)
+	{
+		if (count > letters)
+			break;
+		write(1, &ch, sizeof(char));
+		count++;
+	}
+
+	fclose(file);
+	return (count);
+}
