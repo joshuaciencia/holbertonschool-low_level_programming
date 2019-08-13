ccd63d3da1ecddda979fcc56b262021d5862d292
joshuaciencia <joshuaciencia2016@gmail.com>
2019-08-13 20:40:55 +0000

    close file at error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		close(fd);
 		return (0);
+	}
 
 	bytes = read(fd, buff, letters);
 	if (bytes == -1)
