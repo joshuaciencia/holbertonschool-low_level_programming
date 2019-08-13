f8bd598d35d5344f3286e0c6367b77be6836da30
joshuaciencia <joshuaciencia2016@gmail.com>
2019-08-13 22:13:16 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,28 +17,26 @@ int create_file(const char *filename, char *text_content)
 	int size;
 	char *mem;
 
-	if (!filename || !text_content)
+	if (!filename)
 	{
 		return (-1);
 	}
-
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-
 	if (fd == -1)
 		return (-1);
-
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
 	size = _strlen(text_content);
-
 	mem = malloc(sizeof(char) * size);
-
 	if (!mem)
 	{
 		close(fd);
 		return (-1);
 	}
-
 	w = write(fd, text_content, size);
-
 	if (w == -1)
 	{
 		close(fd);
