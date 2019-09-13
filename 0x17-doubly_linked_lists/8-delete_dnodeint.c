1dd110f17da3a0b0943118518aae8296c6222293
Joshua <joshuaciencia@gmail.com>
2019-09-13 16:09:36 +0000

    added task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 /**
@@ -8,19 +9,46 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	int i = 0;
 	dlistint_t *current;
 
 	if (!*head)
 		return (-1);
 
-	current = *head;
+	current = get_dnodeint_at_index(*head, index);
+	if (!current)
+		return (-1);
+	if (index == 0)
+	{
+		*head = current->next;
+		if (current->next)
+			current->next->prev = 0;
+		free(current);
+		return (1);
+	}
+	current->prev->next = current->next;
+	if (current->next)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
+}
+
+/**
+ * get_dnodeint_at_index - get nth node in a dll
+ * Return: point to nth node
+ * @head: head node
+ * @index: nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+	dlistint_t *current = head;
+
 	while (current)
 	{
-		if (i == index)
-		{
-			
-		}
+		if (counter == index)
+			return (current);
+		counter++;
+		current = current->next;
 	}
-
+	return (0);
 }
