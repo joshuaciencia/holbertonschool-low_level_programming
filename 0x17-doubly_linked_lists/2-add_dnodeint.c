148cf87afd134041ad43ff55f103e47cb83c896d
josh <joshuaciencia2016@gmail.com>
2019-09-13 06:39:09 +0000

    task from 0 to 5

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_dnodeint - add not at the beginning
+ * Return: pointer to node
+ * @h: list
+ * @n: data
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (0);
+
+	new->n = n;
+	new->prev = 0;
+	if (!*head)
+	{
+		new->next = 0;
+		*head = new;
+		return (new);
+	}
+	new->next = *head;
+	(*head)->prev = new;
+	*head = new;
+	return (new);
+}
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - print list
+ * Return: size_t
+ * @h: list
+ */
+
