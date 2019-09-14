8754d4a94e643ad2775cea2d636e864b1762da18
josh <joshuaciencia2016@gmail.com>
2019-09-14 17:55:30 +0000

    fixed for null in 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new, *current;
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (!new || !head)
 		return (0);
 	current = *head;
 
