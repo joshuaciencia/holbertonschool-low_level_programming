4e74a027890a24322c0c223f2463ae4f0eb523b6
josh <joshuaciencia2016@gmail.com>
2019-10-10 22:11:18 +0000

    fixed for update values according to key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,13 +18,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+
+	idx_node = (ht->array)[index];
+
+	if (idx_node && !strcmp(idx_node->key, key))
+	{
+		free(idx_node->value);
+		idx_node->value = strdup(value);
+		return (1);
+	}
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	idx_node = (ht->array)[index];
 
 	(ht->array)[index] = new_node;
 	new_node->next = idx_node;
