0dd3da83c4f5335e8933aa0cd59297e1f09c4940
josh <joshuaciencia2016@gmail.com>
2019-10-15 12:42:09 +0000

    fixed repo mistake

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-class MyList(list):
-    def print_sorted(self):
-        l = self[:]
-        l.sort()
-        print(l)
--- 0x0A-python-inheritance/4-inherits_from.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-
-def inherits_from(obj, a_class):
-    return isinstance(obj, a_class) and not type(obj) == a_class
