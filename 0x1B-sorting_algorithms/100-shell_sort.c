74751b0bf6a618e88244ead8d3824f0811b66765
joshuaciencia <joshuaciencia2016@gmail.com>
2019-10-29 16:07:49 +0000

    shell sort finished

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -1,6 +1,36 @@
 #include "sort.h"
 
-void shell_sort(int *array, size_t size)
+/**
+ * shell_sort - sorts array with ss algorith
+ * @size: size of hash table
+ * @arr: array of ints
+ * Return: void
+ */
+void shell_sort(int *arr, size_t size)
 {
-	
+	int i, j, z;
+	int gaps[] = {701, 301, 132, 57, 23, 10, 4, 1};
+
+	if (!arr || size < 2)
+		return;
+
+	for (z = 0; z < 8; z++)
+	{
+		int gap = gaps[z];
+		int flag = 0;
+
+		for (i = gap; i < (int)size; i++)
+		{
+			flag = 1;
+			for (j = i; j >= gap && arr[j] < arr[j - gap]; j -= gap)
+			{
+				int tmp = arr[j];
+
+				arr[j] = arr[j - gap];
+				arr[j - gap] = tmp;
+			}
+		}
+		if (flag)
+			print_array(arr, size);
+	}
 }
