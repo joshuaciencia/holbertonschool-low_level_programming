3a7dd2d8c141c42e727cbb7ceb0b802fd5abfc0d
josh <joshuaciencia2016@gmail.com>
2019-11-16 02:56:53 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,16 +7,21 @@ Module that contains island perimeter fun
 def island_perimeter(grid):
     """ returns the perimeter of the function
     """
+    if not len(grid):
+        return 0
     per = 0
     for r in range(len(grid)):
         for c in range(len(grid[r])):
             if grid[r][c] == 1:
-                if not grid[r][c - 1]:
-                    per += 1
-                if not grid[r][c + 1]:
-                    per += 1
-                if not grid[r - 1][c]:
-                    per += 1
-                if not grid[r + 1][c]:
-                    per += 1
+                try:
+                    if not grid[r][c - 1]:
+                        per += 1
+                    if not grid[r][c + 1]:
+                        per += 1
+                    if not grid[r - 1][c]:
+                        per += 1
+                    if not grid[r + 1][c]:
+                        per += 1
+                except IndexError:
+                    pass 
     return per
