6858b492d2086c3da7a050422b9ac7ddf4fc50fd
josh <joshuaciencia2016@gmail.com>
2019-12-04 03:57:11 +0000

    levelorder traversal and is_complete tree tasks

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -52,7 +52,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+void traverse_level(const binary_tree_t *tree, void (*func)(int), int level);
 int binary_tree_is_complete(const binary_tree_t *tree);
+int is_complete(const binary_tree_t *tree, int index, int size);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
