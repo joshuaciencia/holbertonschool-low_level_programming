3c9a4bcce50c6ffb6d50dc62873d08ea6f6d5141
josh <joshuaciencia2016@gmail.com>
2019-12-04 23:00:31 +0000

    fix task 1

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -12,8 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (0);
-
 	node = binary_tree_node(parent, value);
+	if (!node)
+		return (0);
 	old_left = parent->left;
 	parent->left = node;
 	node->left = old_left;
