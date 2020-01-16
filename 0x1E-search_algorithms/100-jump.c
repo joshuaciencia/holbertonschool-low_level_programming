266e4d4b4f32166e11b39093306cc24875a7428c
josh <joshuaciencia2016@gmail.com>
2020-01-16 04:01:10 +0000

    task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@ int jump_search(int *array, size_t size, int value)
 	int step = sqrt(size), step_back;
 	int i = 0, j;
 
-	if (!array || !size)
+	if (!array || !size || value < array[0] || value > array[size - 1])
 		return (-1);
 
 	while (i < (int)size)
