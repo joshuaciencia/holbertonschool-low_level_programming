b2d67057d1cc8e63926a49bd887121ac96b81ab4
josh <joshuaciencia2016@gmail.com>
2020-01-16 00:45:02 +0000

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,5 +9,29 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	
+	int left = 0;
+	int right = size - 1;
+	int idx;
+
+	while (left <= right)
+	{
+		int half = (left + right) / 2;
+
+		printf("Searching in array: ");
+		for (idx = left; idx <= right; idx++)
+		{
+			printf("%d", array[idx]);
+			if (idx != right)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		if (array[half] < value)
+			left = half + 1;
+		else if (array[half] > value)
+			right = half - 1;
+		else
+			return (half);
+	}
+	return (-1);
 }
